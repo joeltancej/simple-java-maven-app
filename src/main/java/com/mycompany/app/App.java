b160319879bda90106358b0776eec66bb6b43767
Joel Tan <joel.tan@keysight.com>
2024-02-20 15:57:09 +0800

    removed comments

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,15 +7,12 @@ public class App {
 
     private static final String MESSAGE = "Hello World!";
 
-    // TODO
     public App() {}
-,
-    // TODO
+
     public static void main(String[] args) {
         System.out.println(MESSAGE);
     }
 
-    // TODO
     public String getMessage() {
         return MESSAGE;
     }
